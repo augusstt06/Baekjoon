bf0c46c6cf48c82338c461891dd36fda5888bcdf
ChungYeon Kim <82586703+augusstt06@users.noreply.github.com>
2024-08-22 16:59:44 +0900

    [level 1] Title: K번째수, Time: 4.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,43 +1,45 @@
 function solution(array, commands) {
-    var answer = [];
-    for (let i = 0; i < commands.length; i++){
-        const newArr = mergeSort(array.slice(commands[i][0]-1,commands[i][1]))
-        answer.push(newArr[commands[i][2]-1])
-    }
-    return answer;
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    const  [start, end, index ] = commands[i];
+      console.log(start, end, index)
+    const newArr = mergeSort(array.slice(start - 1, end));
+    answer.push(newArr[index - 1]);
+  }
+  return answer;
 }
 
-function mergeSort(arr){
-    if(arr.length <=1)return arr;
-    
-    const mid = Math.floor(arr.length/2)
-    const left = arr.slice(0,mid)
-    const right = arr.slice(mid)
-    
-    return merge(mergeSort(left), mergeSort(right))
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right){
-    let result = []
-    let leftIndex = 0;
-    let rightIndex = 0;
-    
-    while(leftIndex < left.length && rightIndex < right.length){
-        if (left[leftIndex] < right[rightIndex]){
-            result.push(left[leftIndex])
-            leftIndex++;
-        }else{
-            result.push(right[rightIndex])
-            rightIndex++;
-        }
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
     }
-    while(leftIndex<left.length){
-        result.push(left[leftIndex])
-        leftIndex++;
-    }
-    while(rightIndex<right.length){
-        result.push(right[rightIndex])
-        rightIndex++;
-    }
-    return result
+  }
+  while (leftIndex < left.length) {
+    result.push(left[leftIndex]);
+    leftIndex++;
+  }
+  while (rightIndex < right.length) {
+    result.push(right[rightIndex]);
+    rightIndex++;
+  }
+  return result;
 }
